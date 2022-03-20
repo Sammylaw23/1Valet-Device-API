59093d3cc6ce5d0fb42196c0db0cacd416cfbbbd
Samuel Lawal <salawal@axxess.com>
2022-03-20 18:20:45 -0500

    Added unit test project for API and about to add FakeItEasy library for Unit test mocking

---
+++ tests/OneValet.DeviceGallery.API.UnitTests/DevicesControllerTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace OneValet.DeviceGallery.API.UnitTests
+{
+    public class DevicesControllerTests
+    {
+        [Fact]
+        public void GetDevices_Returns_The_Correct_Number_Of_Devices()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+        }
+    }
+}
+
+//Arrange
+//Act
+//Assert 
